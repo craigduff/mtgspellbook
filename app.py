1a4e6915017bce969ab1f03e15226f9dcf62c202
Craig Duff <craig.duff7@mail.dcu.ie>
2018-03-29 16:18:40 -0700

    updated app.py

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def builder():
 	return "hello world"
+
+
+@app.route('/spellbook')
+def spellbook():
+	return "spellbook built!"
+
 
 if __name__ == '__main__':
 	app.run()
