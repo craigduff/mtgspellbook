723aed8bd652b4a5239c557409c415b90e34488b
Craig Duff <craig.duff7@mail.dcu.ie>
2018-03-31 15:30:26 -0700

    added input form

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from config import Config
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+class TextForm(FlaskForm):
+	textform = StringField('cards', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 @app.route('/')
 def builder():
-	return "hello world"
+	form = TextForm()
+	return render_template('home.html', title='Submission', form=form)
 
 
-@app.route('/spellbook')
+@app.route('/spellbook', methods=['POST'])
 def spellbook():
 	return "spellbook built!"
 
