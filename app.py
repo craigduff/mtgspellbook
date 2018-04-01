993d68cc2cc101eb32870c250b112e4026ca2edb
Craig Duff <craig.duff7@mail.dcu.ie>
2018-04-01 12:46:19 -0700

    start parsing form data

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template, redirect, request
 from config import Config
-
+import json
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 class TextForm(FlaskForm):
-	textform = StringField('cards', validators=[DataRequired()])
+	textform = TextAreaField('cards', validators=[DataRequired()])
 	submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def builder():
 	form = TextForm()
+	if request.method == 'POST':
+		return request.form.to_dict(flat=False)['textform'][0]
+		redirect('home.html')
 	return render_template('home.html', title='Submission', form=form)
 
 
-@app.route('/spellbook', methods=['POST'])
+@app.route('/spellbook')
 def spellbook():
 	return "spellbook built!"
 
